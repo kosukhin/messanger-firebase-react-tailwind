b3cd2f385aa7464c7b3c7b8347efdedfe844a26f
Eugene Kosukhin <kosuha606@gmail.com>
2023-11-07 09:01:29 +0300

    feat(main): Добавить иконку профиля в хедер

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,23 @@
+import {useEffect, useState} from "react";
+import {takeInstance} from "../../modules/base/I";
+import {User} from "../../modules/user/User";
+import {takeServices} from "../../modules/base/takeServices";
+
 function Header() {
+  const [user, setUser] = useState(takeInstance(User, '', '', '', ''))
+
+  useEffect(() => {
+    const {users} = takeServices()
+    users.currentUser().then(user => {
+      setUser(user)
+    })
+  }, [])
+
   return (<header className="p-4 bg-header flex gap-2">
     <a href="/">Firebase-messenger</a>
     <a className={'block ml-auto'} href="/messages">Все сообщения</a>
     <a className={'block'} href="/profile">Профиль</a>
+    <img className="w-6 h-6 rounded-full" src={user.avatar} alt="Rounded avatar"/>
   </header>)
 }
 
