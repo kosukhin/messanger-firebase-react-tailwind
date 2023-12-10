e6f1491ee0724021ad9ef802870edd4f500f2af8
Eugene Kosukhin <kosuha606@gmail.com>
2023-12-10 17:11:29 +0300

    feat: Доработать ридонли

--- src/modules/base/I.ts
+++ src/modules/base/I.ts
@@ -12,7 +12,7 @@ export const defineModelFactory = <T,>() => <D extends Partial<T>>(defaults?: D)
 }
 
 export const defineModelEffect = <T extends (...args: any[]) => any, R extends any>(modelFactory: T, fn: (model: ReturnType<T>) => Promise<R>) => {
-  return async <CR = void>(...args: Parameters<T>): Promise<CR extends void ? R : CR> => {
+  return async <CR = void>(...args: Parameters<T>): Promise<Readonly<CR extends void ? R : CR>> => {
     const model = modelFactory(...args);
     return fn(model) as CR extends void ? R : CR
   }
